da73094d8ceb1dd55d64c4ada00e3cef4c2a48a6
WenyanJIAO <91078531+WenyanJIAO@users.noreply.github.com>
2021-09-29 20:41:41 +0100

    Update sp06.R

--- sp06.R
+++ sp06.R
@@ -3,32 +3,22 @@ a <- scan("1581-0.txt",what="character",skip=156)
 n <- length(a)
 a <- a[-((n-2909):n)] ## strip license
 
-m<-grep(",",a,fixed=TRUE)
-a[m]<-paste(a[m]," A",sep="")
-k<-paste(a,collapse=" ")
-pow1 <- strsplit(k," ")[[1]]
-t<-gsub(",", "", pow1, ignore.case = FALSE, perl = FALSE,
-        fixed = TRUE, useBytes = FALSE)
-t1<-gsub("A", ",", t, ignore.case = FALSE, perl = FALSE,
-         fixed = TRUE, useBytes = FALSE)
+split_punct <- function(book,string){
+  m <- grep(string, book, fixed=TRUE)
+  book[m] <- paste(book[m], " %", sep="")
+  n <- paste(book, collapse=" ")
+  p <- strsplit(n," ")[[1]]
+  t <- gsub(string, "", p, ignore.case = FALSE, perl = FALSE,
+          fixed = TRUE, useBytes = FALSE)
+  t1 <- gsub("%", string, t, ignore.case = FALSE, perl = FALSE,
+           fixed = TRUE, useBytes = FALSE)
+  return (t1)
+}
 
-m<-grep(".",t1,fixed=TRUE)
-t1[m]<-paste(t1[m]," A",sep="")
-k<-paste(t1,collapse=" ")
-pow1 <- strsplit(k," ")[[1]]
-t<-gsub(".", "", pow1, ignore.case = FALSE, perl = FALSE,
-        fixed = TRUE, useBytes = FALSE)
-t2<-gsub("A", ".", t, ignore.case = FALSE, perl = FALSE,
-         fixed = TRUE, useBytes = FALSE)
+del_1<-split_punct(a,',')
+del_2<-split_punct(del_1,'.')
+del_3<-split_punct(del_2,';')
+del_4<-split_punct(del_3,'!')
+del_5<-split_punct(del_4,':')
+del_6<-split_punct(del_5,"?")
 
-
-m<-grep(":",t2,fixed=TRUE)
-
-t2[m]<-paste(t2[m]," A",sep="")
-k<-paste(t2,collapse=" ")
-a1 <- strsplit(k," ")[[1]]
-t<-gsub(":", "", a1, ignore.case = FALSE, perl = FALSE,
-        fixed = TRUE, useBytes = FALSE)
-t3<-gsub("A", ":", t, ignore.case = FALSE, perl = FALSE,
-         fixed = TRUE, useBytes = FALSE)
-t3
