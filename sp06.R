7caffb770fcf85a8e3727d4a5046d7cac05c95a0
86132 <your email>
2021-09-30 19:04:47 +0100

    102-11

--- sp06.R
+++ sp06.R
@@ -16,10 +16,23 @@ split_punct <- function(book,string){
 }
 
 
-del_11<-split_punct(a,',')
+del_1<-split_punct(a,',')
 del_2<-split_punct(del_1,'.')
 del_3<-split_punct(del_2,';')
 del_4<-split_punct(del_3,'!')
 del_5<-split_punct(del_4,':')
 del_6<-split_punct(del_5,"?")
 
+library(mgcv)
+
+text <- tolower(del_6)
+k <- uniquecombs(text, ordered = FALSE)
+k1<-attr(k, "index")
+
+
+# X <- matrix(c(1,2,3,1,2,3,4,5,6,1,3,2,4,5,6,1,1,1),6,3,byrow=TRUE)
+# print(X)
+# Xu <- uniquecombs(X);Xu
+# ind <- attr(Xu,"index")
+# ## find the value for row 3 of the original from Xu
+# Xu[ind[3],];X[3,]
